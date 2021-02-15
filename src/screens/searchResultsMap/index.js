255a0956d68e36c78598e6a81a978ecf3325a2d1
Juan Camilo Chafloque M <camilochafloque@gmail.com>
2021-02-15 15:07:11 -0500

    Carousel Post Item Functionalities and Map View Finished

--- src/screens/searchResultsMap/index.js
+++ src/screens/searchResultsMap/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { View, Text, FlatList } from 'react-native'
 import MapView, { Marker } from 'react-native-maps';
 import styles from '../searchResultsMap/styles';
@@ -11,10 +11,39 @@ const SearchResultsMapScreen = (props) => {
 
     const [selectedPlaceId, setSelectedPlaceId] = useState(null);
     const width = useWindowDimensions().width;
+    const refFlatList = useRef(null);
+    const viewConfig = useRef({ itemVisiblePercentThreshold: 70 });
+    const onViewChanged = useRef(({ viewableItems }) => {
+        if (viewableItems.length > 0) {
+            const selectedPlace = viewableItems[0].item;
+            setSelectedPlaceId(selectedPlace.id);
+        }
+    });
+    const map = useRef();
+
+    useEffect(() => {
+        if (!selectedPlaceId || !refFlatList) {
+            return;
+        }
+        const index = places.findIndex(place => place.id === selectedPlaceId);
+        refFlatList.current.scrollToIndex({ index });
+        const selectedPlace = places[index];
+        const region = {
+            latitude: selectedPlace.coordinate.latitude,
+            longitude: selectedPlace.coordinate.longitude,
+            latitudeDelta: 0.8,
+            longitudeDelta: 0.8
+        }
+
+        map.current.animateToRegion(region);
+
+
+    }, [selectedPlaceId])
 
     return (
         <View style={styles.container} >
             <MapView
+                ref={map}
                 style={styles.map}
                 initialRegion={{
                     latitude: 28.3279822,
@@ -34,13 +63,16 @@ const SearchResultsMapScreen = (props) => {
 
             <View style={styles.items}>
                 <FlatList
+                    ref={refFlatList}
                     data={places}
                     renderItem={({ item }) => <PostCarouselItem post={item} />}
                     horizontal
                     showsHorizontalScrollIndicator={false}
                     snapToInterval={width - 60}
                     snapToAlignment={'center'}
-                    decelerationRate={'fast'} />
+                    decelerationRate={'fast'}
+                    viewabilityConfig={viewConfig.current}
+                    onViewableItemsChanged={onViewChanged.current} />
             </View>
         </View>
     )
